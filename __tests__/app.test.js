db02cd9c63b0994a367c81505bcdf877bf40bd2d
Kalan Prudhomme <cleaver9@gmail.com>
2021-11-05 14:05:09 -0700

    wrote first test, first model and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,26 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Problem = require('../lib/models/Problem.js');
+
 
 describe('Duck-BE routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  const problem = {
+    name: 'Function is broken',
+    description: 'It does not work',
+    code: '<Hello World!>'
+  };
+
+  test('create a problem', async () => {
+    const res = await request(app)
+      .post('.api/v1/problems')
+      .send(problem);
+
+    expect(res.body).toEqual({ ...problem, id: '1' });
   });
 
   afterAll(() => {
