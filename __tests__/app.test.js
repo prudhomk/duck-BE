b74caad673ed9f5cde31dfd8bea5c4decef4936c
Kalan Prudhomme <cleaver9@gmail.com>
2021-11-19 13:52:30 -0800

    passing tests for create and find by desc for problem and solution

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Problem from '../lib/models/Problem.js';
+import Solution from '../lib/models/Solution.js';
 
 
 describe('Duck-BE routes', () => {
@@ -13,6 +14,12 @@ describe('Duck-BE routes', () => {
   const problem = {
     description: 'It does not render',
     code: '<Hello World!>'
+  };
+
+  const solution = {
+    description: 'I fixed it',
+    code: '<h1>Hello World!</h1>',
+    problem: '1'
   };
 
   test('create a problem', async () => {
@@ -30,6 +37,26 @@ describe('Duck-BE routes', () => {
       .get('/api/v1/problems/description?description=render');
     
     expect(res.body).toEqual([newProblem]);
+  });
+
+  test('creates a solution', async () => {
+    const newProblem = await Problem.create(problem);
+
+    const res = await request(app)
+      .post('/api/v1/solutions')
+      .send({ ...solution, problem: newProblem.id });
+
+    expect(res.body).toEqual({ ...solution, id: '1' });
+  });
+
+  test('retrieves a solution by description', async () => {
+    const newProblem = await Problem.create(problem);
+    const newSolution = await Solution.create(solution);
+
+    const res = await request(app)
+      .get('/api/v1/solutions/description?description=fixed');
+
+    expect(res.body).toEqual([newSolution]);
   });
 
   afterAll(() => {
